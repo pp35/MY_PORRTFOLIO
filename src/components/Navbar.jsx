ddae4b7f433f090fba0ef8c0634238e84cee4008
pp35 <poornimapandey3590@gmail.com>
2023-12-03 23:50:04 +0530

    fff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import { CgMenuGridR } from 'react-icons/cg';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
 
-  
-
   const mobileNavContent = (
-    <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
-      <ul className="text-center text-xl p-20">
-        <Link to="*">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-500 cursor-pointer hover:rounded">Home</li>
-        </Link>
-        <Link to="/About">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-500 cursor-pointer hover:rounded">About</li>
-        </Link>
-        <Link to="/Services">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-500 cursor-pointer hover:rounded">Services</li>
-        </Link>
-        <Link to="/Projects">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-500 cursor-pointer hover:rounded">Projects</li>
-        </Link>
-        <Link to="/Contact">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-500 cursor-pointer hover:rounded">Contact</li>
-        </Link>
-      </ul>
+    <div className="lg:hidden fixed top-0 left-0 w-full h-full bg-slate-900 z-50">
+      <div className="flex justify-center items-center h-full">
+        <ul className="text-center text-xl">
+          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-500 cursor-pointer hover:rounded">
+            <NavLink to="/" onClick={handleClick}>Home</NavLink>
+          </li>
+          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-500 cursor-pointer hover:rounded">
+            <NavLink to="/About" onClick={handleClick}>About</NavLink>
+          </li>
+          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-500 cursor-pointer hover:rounded">
+            <NavLink to="/Services" onClick={handleClick}>Services</NavLink>
+          </li>
+          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-500 cursor-pointer hover:rounded">
+            <NavLink to="/Projects" onClick={handleClick}>Projects</NavLink>
+          </li>
+          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-500 cursor-pointer hover:rounded">
+            <NavLink to="/Contact" onClick={handleClick}>Contact</NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 
@@ -41,15 +41,15 @@ const Navbar = () => {
         <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden">
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[18px]">
-              <Link to="/">Home</Link>
-              <Link to="/About">About</Link>
-              <Link to="/Services">Services</Link>
-              <Link to="/Projects">Projects</Link>
-              <Link to="/Contact">Contact</Link>
+              <NavLink to="/" activeClassName="text-blue-500">Home</NavLink>
+              <NavLink to="/About" activeClassName="text-blue-500">About</NavLink>
+              <NavLink to="/Services" activeClassName="text-blue-500">Services</NavLink>
+              <NavLink to="/Projects" activeClassName="text-blue-500">Projects</NavLink>
+              <NavLink to="/Contact" activeClassName="text-blue-500">Contact</NavLink>
             </ul>
           </div>
         </div>
-        <div>{click && mobileNavContent}</div>
+        {click ? mobileNavContent : null}
         <button className="block sm:hidden transition" onClick={handleClick}>
           {click ? <FaTimes /> : <CgMenuGridR />}
         </button>
